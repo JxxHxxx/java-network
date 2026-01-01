cd945d8d59a264bcd0e377ad10bead0422008d69
JxxHxxx <leesin5498@naver.com>
2026-01-01 15:04:50 +0900

    소켓 통신 초기 샘플 예제

---
+++ src/network/tcp/v1/ClientV1.java
@@ -0,0 +1,38 @@
+package network.tcp.v1;
+
+import util.Logger;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static util.Logger.*;
+
+public class ClientV1 {
+    private static final int PORT =12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        // 소켓 연결이 되면 자동으로 클라이언트 포트가 할당된다.
+        Socket socket = new Socket("localhost", PORT);
+
+
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        log("소캣 연결: " + socket);
+
+        // 서버에게 문자 보내기
+        String toSend = "hello";
+        output.writeUTF(toSend);
+        log("client -> server: " + toSend);
+        // 서버로부터 문자 받기
+        String received = input.readUTF();
+        log("client <- server: " + received);
+
+        // 자원 정리
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
---
+++ src/network/tcp/v2/ClientV2.java
@@ -0,0 +1,47 @@
+package network.tcp.v2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static util.Logger.log;
+
+public class ClientV2 {
+    private static final int PORT =12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        // 소켓 연결이 되면 자동으로 클라이언트 포트가 할당된다.
+        Socket socket = new Socket("localhost", PORT);
+
+
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        log("소캣 연결: " + socket);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("전송 문자: ");
+            String toSend = scanner.next();
+
+            // 서버에게 문자 보내기
+            output.writeUTF(toSend);
+            log("client -> server: " + toSend);
+
+            if (toSend.equals("exit")) {
+                break;
+            }
+            // 서버로부터 문자 받기
+            String received = input.readUTF();
+            log("client <- server: " + received);
+        }
+
+
+        // 자원 정리
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
