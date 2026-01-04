c08a2bf75f2b25f1f73777c5f38d300e9ac6f228
JxxHxxx <leesin5498@naver.com>
2026-01-04 16:47:25 +0900

    feat: readTimeout 테스트 할 수 있는 클라/서버 추가

---
+++ src/network/tcp/v10/ServerV10.java
@@ -0,0 +1,50 @@
+package network.tcp.v10;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static util.Logger.log;
+
+public class ServerV10 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        log("서버 시작");
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        log("서버 소켓 시작 - 리스닝 포트: " + PORT);
+
+        Socket socket = serverSocket.accept(); // 블로킹
+        log("소켓 연결: " + socket);
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+        int delay = 1000;
+        while (true) {
+            // 클라이언트로부터 문자 받기
+            String received = input.readUTF(); //  블로킹
+
+            Thread.sleep(delay);
+            delay = delay + 1000;
+            log("client -> server: " + received);
+
+            if (received.equals("exit")) {
+                break;
+            }
+
+            // 클라리언트에게 문자 보내기
+            String toSend = received + " World!";
+            output.writeUTF(toSend);
+            // 자원 정리
+            log("연결 종료: " + socket);
+        }
+
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
